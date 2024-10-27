72238b96a469a4bc7541e28f57fd2954ab50026a
zainab-senhaji-mimidi <130577375+zainab-senhaji-mimidi@users.noreply.github.com>
2024-10-27 18:38:48 +0100

    Create bdd.sql

---
+++ data/bdd.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 27 oct. 2024 à 18:38
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `omeka-z`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `resource`
+--
+
+CREATE TABLE `resource` (
+  `id` int(11) NOT NULL,
+  `owner_id` int(11) DEFAULT NULL,
+  `resource_class_id` int(11) DEFAULT NULL,
+  `resource_template_id` int(11) DEFAULT NULL,
+  `thumbnail_id` int(11) DEFAULT NULL,
+  `title` longtext DEFAULT NULL,
+  `is_public` tinyint(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
+  `resource_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `resource`
+--
+
+INSERT INTO `resource` (`id`, `owner_id`, `resource_class_id`, `resource_template_id`, `thumbnail_id`, `title`, `is_public`, `created`, `modified`, `resource_type`) VALUES
+(1, 1, 106, 2, NULL, 'locateur 1', 1, '2024-10-27 17:29:42', '2024-10-27 17:32:18', 'Omeka\\Entity\\Item'),
+(2, 1, 106, 2, NULL, 'locateur 2', 1, '2024-10-27 17:31:52', '2024-10-27 17:31:52', 'Omeka\\Entity\\Item');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `resource`
+--
+ALTER TABLE `resource`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_BC91F4167E3C61F9` (`owner_id`),
+  ADD KEY `IDX_BC91F416448CC1BD` (`resource_class_id`),
+  ADD KEY `IDX_BC91F41616131EA` (`resource_template_id`),
+  ADD KEY `IDX_BC91F416FDFF2E92` (`thumbnail_id`),
+  ADD KEY `title` (`title`(190)),
+  ADD KEY `is_public` (`is_public`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `resource`
+--
+ALTER TABLE `resource`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `resource`
+--
+ALTER TABLE `resource`
+  ADD CONSTRAINT `FK_BC91F41616131EA` FOREIGN KEY (`resource_template_id`) REFERENCES `resource_template` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_BC91F416448CC1BD` FOREIGN KEY (`resource_class_id`) REFERENCES `resource_class` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_BC91F4167E3C61F9` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_BC91F416FDFF2E92` FOREIGN KEY (`thumbnail_id`) REFERENCES `asset` (`id`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
